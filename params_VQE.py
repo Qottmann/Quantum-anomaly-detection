d22670a81d184a9cfb829ef78eb24961a55e7a87
Qottmann <Korbinian.Kottmann@gmail.com>
2021-05-21 14:06:44 +0200

    small changes + corrected parameters

--- params_VQE.py
+++ params_VQE.py
@@ -6,17 +6,18 @@ from qiskit.opflow.state_fns import StateFn, CircuitStateFn
 from qiskit.providers.aer import StatevectorSimulator, AerSimulator
 from qiskit.algorithms import VQE
 from qiskit.algorithms.optimizers import COBYLA, SLSQP, SPSA
-import matplotlib.pyplot as plt
 from scipy import sparse
 import scipy.sparse.linalg.eigen.arpack as arp
 from modules.utils import *
 
-for anti in [1., -1.]:
+for anti in [-1.]:
 
-    L = 6
-
-    gx_vals = np.logspace(-2,2,10)
-    gz_vals = np.logspace(-2,2,10)
+    L = 5
+    logspace_size = 50
+    filename = f'data/params_VQE_Ising_L{L:.0f}_anti_{anti:.0f}_{logspace_size}x{logspace_size}.npz'
+    print(filename)
+    gx_vals = np.logspace(-2,2,logspace_size)
+    gz_vals = np.logspace(-2,2,logspace_size)
 
     backend = StatevectorSimulator()
     optimizer = SLSQP(maxiter=1000)
@@ -44,4 +45,4 @@ for anti in [1., -1.]:
             gz_list.append(gz)
             opt_params.append(sort_params(result.optimal_parameters))
 
-    np.savez(f'params_VQE_Ising_L%.0f_anti_%.0f.npz' %(L,anti), gx_list=gx_list, gz_list=gz_list, opt_params=opt_params)
+    np.savez(filename, gx_list=gx_list, gz_list=gz_list, opt_params=opt_params)
